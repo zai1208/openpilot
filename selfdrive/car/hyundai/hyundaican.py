db31a8c0895571b468e28e9fe89fcab1216d78c1
Shane Smiskol <shane@smiskol.com>
2022-02-10 20:15:07 -0800

    increment current counter

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -62,10 +62,10 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
   return packer.make_can_msg("LKAS11", 0, values)
 
 
-def create_clu11(packer, frame, clu11, button):
+def create_clu11(packer, clu11, button):
   values = clu11
   values["CF_Clu_CruiseSwState"] = button
-  values["CF_Clu_AliveCnt1"] = frame % 0x10
+  values["CF_Clu_AliveCnt1"] += 1
   return packer.make_can_msg("CLU11", 0, values)
 
 
