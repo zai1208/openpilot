54db134b232500abce4273c4468dc60469223212
Shane Smiskol <shane@smiskol.com>
2022-03-03 02:56:44 -0800

    revert nice to haves

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -144,31 +144,31 @@ function launch {
   # 2. The FINALIZED consistent file has to exist, indicating there's an update
   #    that completed successfully and synced to disk.
 
-#  if [ -f "${BASEDIR}/.overlay_init" ]; then
-#    find ${BASEDIR}/.git -newer ${BASEDIR}/.overlay_init | grep -q '.' 2> /dev/null
-#    if [ $? -eq 0 ]; then
-#      echo "${BASEDIR} has been modified, skipping overlay update installation"
-#    else
-#      if [ -f "${STAGING_ROOT}/finalized/.overlay_consistent" ]; then
-#        if [ ! -d /data/safe_staging/old_openpilot ]; then
-#          echo "Valid overlay update found, installing"
-#          LAUNCHER_LOCATION="${BASH_SOURCE[0]}"
-#
-#          mv $BASEDIR /data/safe_staging/old_openpilot
-#          mv "${STAGING_ROOT}/finalized" $BASEDIR
-#          cd $BASEDIR
-#
-#          echo "Restarting launch script ${LAUNCHER_LOCATION}"
-#          unset REQUIRED_NEOS_VERSION
-#          unset AGNOS_VERSION
-#          exec "${LAUNCHER_LOCATION}"
-#        else
-#          echo "openpilot backup found, not updating"
-#          # TODO: restore backup? This means the updater didn't start after swapping
-#        fi
-#      fi
-#    fi
-#  fi
+  if [ -f "${BASEDIR}/.overlay_init" ]; then
+    find ${BASEDIR}/.git -newer ${BASEDIR}/.overlay_init | grep -q '.' 2> /dev/null
+    if [ $? -eq 0 ]; then
+      echo "${BASEDIR} has been modified, skipping overlay update installation"
+    else
+      if [ -f "${STAGING_ROOT}/finalized/.overlay_consistent" ]; then
+        if [ ! -d /data/safe_staging/old_openpilot ]; then
+          echo "Valid overlay update found, installing"
+          LAUNCHER_LOCATION="${BASH_SOURCE[0]}"
+
+          mv $BASEDIR /data/safe_staging/old_openpilot
+          mv "${STAGING_ROOT}/finalized" $BASEDIR
+          cd $BASEDIR
+
+          echo "Restarting launch script ${LAUNCHER_LOCATION}"
+          unset REQUIRED_NEOS_VERSION
+          unset AGNOS_VERSION
+          exec "${LAUNCHER_LOCATION}"
+        else
+          echo "openpilot backup found, not updating"
+          # TODO: restore backup? This means the updater didn't start after swapping
+        fi
+      fi
+    fi
+  fi
 
   # handle pythonpath
   ln -sfn $(pwd) /data/pythonpath
